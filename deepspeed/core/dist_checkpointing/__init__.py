ad0c8baa45a949ea7967cdcc07df61977cfed430
dylanyunlon <dogechat@163.com>
2026-06-12 11:24:33 +0000

    M1437: Megatron 69bcb1040 — Shorten license headers
    Signed-off-by: dylanyunlon <dogechat@163.com>

---
+++ deepspeed/core/dist_checkpointing/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
+
+from .core import check_is_distributed_checkpoint
+from .mapping import ShardedTensor, LocalNonpersitentObject
+from .serialization import load, save, load_common_state_dict
+print('[M1437]')
---
+++ deepspeed/core/dist_checkpointing/strategies/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
+
+""" Various loading and saving strategies """
+
+try:
+    import zarr
+    import tensorstore
+    from .zarr import _import_trigger
+    from .tensorstore import _import_trigger
+except ImportError:
+    print('Zarr strategies will not be registered because of missing packages')
+print('[M1437]')
