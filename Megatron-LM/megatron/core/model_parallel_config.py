4f3cb2b551a961863a9a63d5e4237c509df9f84c
dylanyunlon <dogechat@163.com>
2026-06-15 11:14:47 +0000

    M2427: Megatron 5db6704d8 — ADLR/megatron-lm!4172 - Typo fixes from community
    Signed-off-by: dylanyunlon <dogechat@163.com>

--- Megatron-LM/megatron/core/model_parallel_config.py
+++ Megatron-LM/megatron/core/model_parallel_config.py
@@ -337,6 +337,9 @@ class ModelParallelConfig:
     cpu_offloading_weights: bool = False
     """If True, offloads the weights to CPU."""
 
+    cpu_offloading_double_buffering: bool = False
+    """If True, enables double buffering across layers while reloading activations from CPU."""
+
     ###################
     # Timing
     ###################
@@ -353,7 +356,7 @@ class ModelParallelConfig:
         """
         if self.sequence_parallel:
             if self.tensor_model_parallel_size <= 1:
-                raise ValueError("Can not use sequence paralllelism without tensor parallelism")
+                raise ValueError("Cannot use sequence parallelism without tensor parallelism")
 
         if self.expert_tensor_parallel_size is None:
             self.expert_tensor_parallel_size = self.tensor_model_parallel_size
